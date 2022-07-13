6c0f6b5317ef4081c6099cc95186040593cfa2c2
Jay Slade <brandonslade2@gmail.com>
2022-07-12 18:14:58 -0700

    addtoZero Finish

--- addToZero.js
+++ addToZero.js
@@ -2,3 +2,53 @@
 let array = [28, 43, -12, 30, 4, 0, 12]
 
 // Write your solution below:
+
+
+let solution = "false"
+
+
+
+for (let i = 0; i < array.length; i++) {
+    for (let o = 0; o < array.length; o++) {
+        if (i !== o) {
+            if (array[i] + array[o] === 0) {
+solution = "true"
+            }
+        }
+    }
+}
+
+
+
+console.log(solution)
+
+
+//THE ONLY THING THATS GETTING IN THE WAY IS THESE BRACKETS. IM TESTING AND FAILING ONLY TO FIND IN THE END OF THIS THAT IT HAS ALWAYS BEEN A BRACKET PROBLEM!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+
+
+/*let j = 1
+
+for (i = 0; i < array.length; i++) {
+} if (array[i] + array[i + j] === 0) {
+    solution = 'true'
+} else {
+        j += 1
+
+}
+
+console.log(solution)*/
+
+
+/*let y = 1
+
+for (i = 0; i < array.length; i++) {
+} if (array[i] + array[i + y] != 0) {
+    solution = 'false'
+    y += 1
+} if (array[i] + array[i + y] === 0) {
+    break
+}
+
+console.log(solution)*/
